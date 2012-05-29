e80f6929ddc73536dcf2a3fb515a2fe1c8fa9125
skamander <michalniec@gmail.com>
2012-05-30 01:51:04 +0200

    fix - missing header added - regex.h

--- FilesStatistics/daemon/main.c
+++ FilesStatistics/daemon/main.c
@@ -11,6 +11,7 @@
 #include <time.h>
 #include <syslog.h>
 #include <time.h>
+#include <regex.h>
 
 #define EVENT_SIZE  ( sizeof (struct inotify_event) )
 #define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
